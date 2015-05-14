0072f2f933bc3ffc248bf439d5d0eaa1fb172b73
Selvakumar Ponnusamy <selvakumar@modria.com>
2015-05-14 20:21:39 +0530

    Event Handler,Ajax and other versions added

--- src/jquery.tagcloud.js
+++ src/jquery.tagcloud.js
@@ -7,21 +7,81 @@
 		
 		var opts = $.extend({}, $.fn.tagcloud.defaults, options);
 		var container = this;
-		var source = opts.source;
+		var source = $.isFunction(opts.source)?opts.source():opts.source;
+		var maxVal = 0;
+		var r = 0;
+		if(opts.colors){
+			var colorCount = opts.colors.length;
+			if(colorCount>0 && colorCount<100){
+				r = 100/colorCount;
+			}
+		}
+		
 		this.addClass("tagcloud");
 		$.each(source, function(idx, obj) {
+			if($.isNumeric(obj.weight) && Number(obj.weight)>maxVal){
+				maxVal = Number(obj.weight);
+			}
+		});
+		
+		$.each(source, function(idx, obj) {
+			
 			var span = $(document.createElement("span"));
-			span.text(obj.name);
-			span.css("font-size", (obj.weight * opts.weightage) + "%");
+			var per = (obj.weight/maxVal)*100;
+			var color = getColor(per,r);
+			var css = {};
+			css["font-size"] = (((obj.weight/maxVal)*100) * opts.weightage) + "%"; 
+			if(color){
+				css["color"] = color;
+			}
+			if(opts.handlers){
+				$.each(opts.handlers,function(key, value){
+					span.bind(key,value);
+				});
+			}
+			
+			if(obj.link != undefined){
+				var link = $(document.createElement("a"));
+				link.attr("href", obj.link);
+				link.text(obj.name);
+				
+				$.each(css,function(key, value){
+					link.css(key,value);	
+				});
+				
+				span.append(link);
+			}else{
+				span.text(obj.name);
+				$.each(css,function(key, value){
+					span.css(key,value);	
+				});
+			}
+			
+			
 			container.append(span);
 			span.after(" ");
 		});
+		
+		function getColor(per,r){
+			var range = 0;
+			var colors = opts.colors;
+			if(colors && r && per){
+				for(var i=0;i<colors.length;i++){
+					if(per>range && per<=(range+r)){
+						return colors[i];
+					}else{
+						range += r;
+					}
+				}
+			}
+		}
+		
 		return this;
 	};
 
 	
 	$.fn.tagcloud.defaults = {
-			weightage:2
+			weightage:5
 	};
 
 }(jQuery));
